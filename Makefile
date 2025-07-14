17770e1c0341b927102a2d7c5977ea2184bfda59
xeff09 <wongchaita03@gmail.com>
2025-07-14 16:47:51 +0700

    first commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+build:
+	@ echo "Building application... "
+	@ go build -trimpath -o ./bin/engine ./app/
+	@ echo "Done!"
+
+build-migrate:
+	@ echo "Building migrate... "
+	@ go build -trimpath -o ./bin/migrate ./command/migrate.db.go
+	@ echo "Done!"
+
+build-image:
+	@ echo "Bualding docker amage..."
+	@ docker build \
+		--file ./infra/docker/prod.Dockerfile \
+		--tag xeff09/decauth-be \
+		.
+	@ echo "Done!"
+
+# container env (default)
+
+up:
+	@ docker compose up
+
+follow:
+	@ docker logs decauth-app-1 --follow
+
+migrate:
+	@ docker exec -it decauth-app-1 go run ./command/migrate.db.go 
---
+++ app/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func setup() *fiber.App {
+	app := fiber.New()
+	// cfg := config.NewConfig()
+	// db := bootstrap.NewDB(&cfg)
+	// ctx := ctx.ProvideContext()
+
+	app.Use(logger.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000", // Allow requests from the frontend
+		AllowMethods: "GET,POST,PUT,DELETE",   // Allow specific HTTP methods
+		AllowHeaders: "Content-Type,Authorization",
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("decauth is running 🚀")
+	})
+
+	// TODO: auth
+	// oauthRepoFactory := repository.NewOAuthRepositoryFactory()
+	// oauthRepoFactory.Register("google", oauth.NewGoogleOAuthRepository(&cfg))
+	//
+	// userRepo := gorm.NewUserGormRepository(db)
+	// userUsecase := usecase.NewUserUsecase(userRepo)
+	// _userHandler := rest.NewUserHandler(userUsecase)
+	//
+	// authUsecase := usecase.NewAuthUsecase(userRepo, &oauthRepoFactory, &cfg, minioRepo, ctx)
+	// authHandler := rest.NewAuthHandler(authUsecase)
+	//
+	//
+	// authRoute := app.Group("/auth")
+	// authRoute.Post("/login", authHandler.LoginWithCredentials)
+	// authRoute.Post("/login/oauth", authHandler.LoginWithOAuth)
+	// authRoute.Post("/register", authHandler.Register)
+
+	return app
+}
+
+func main() {
+	app := setup()
+	app.Listen(":9090")
+}
